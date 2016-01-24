af50be0ab143d106230dfa25ce90d55ab0123906
Avaer <a@kazmer.io>
2016-01-24 23:24:55 +0000

    Initial Node.removeBlock method implementation

--- public/records/node/index.js
+++ public/records/node/index.js
@@ -3,6 +3,24 @@ import Immutable from 'immutable';
 import * as jsUtils from '../../utils/js/index';
 import Vector from '../vector/index';
 
+const AXIS_DIMENSIONS = {
+  HORIZONTAL: {
+    'xy': 'x',
+    'yz': 'z',
+    'xz': 'x',
+  },
+  VERTICAL: {
+    'xy': 'y',
+    'yz': 'y',
+    'xz': 'z',
+  },
+  DEPTH: {
+    'xy': 'z',
+    'yz': 'x',
+    'xz': 'y',
+  }
+};
+
 export default class Node extends Immutable.Record({
   id: '',
   position: new Vector(),
@@ -24,6 +42,51 @@ export default class Node extends Immutable.Record({
     } else {
       return null;
     }
+  }
+
+  getVolume() {
+    let {x, y, z} = box;
+    return (x + 1) * (y + 1) * (z + 1);
+  }
+
+  removeBlock(removeCoords) {
+    const axis = this.getAxis();
+
+    const horizontalDimension = AXIS_DIMENSIONS.HORIZONTAL[axis];
+    const hPosition = this.getIn(['position', horizontalDimension]);
+    const hBox = this.getIn(['box', horizontalDimension]);
+    const hRemove = removeCoords.get(horizontalDimension);
+
+    const verticalDimension = AXIS_DIMENSIONS.VERTICAL[axis];
+    const vPosition = this.getIn(['position', verticalDimension]);
+    const vBox = this.getIn(['box', verticalDimension]);
+    const vRemove = removeCoords.get(verticalDimension);
+
+    const depthDimension = AXIS_DIMENSIONS.DEPTH[axis];
+    const dPosition = this.getIn(['position', depthDimension]);
+    const dBox = this.getIn(['box', depthDimension]);
+
+    const makeSlicedNode = (h1, v1, h2, v2) => {
+      const startCoords = new Vector()
+        .set(horizontalDimension, h1)
+        .set(verticalDimension, v1)
+        .set(depthDimension, dPosition);
+      const endCoords = new Vector()
+        .set(horizontalDimension, h2)
+        .set(verticalDimension, v2)
+        .set(depthDimension, dPosition + dBox);
+      return Node.fromCoords({startCoords, endCoords});
+	};
+
+    const topLeft = makeSlicedNode(hPosition, vRemove, hRemove - 1, vRemove + vBox);
+    const topRight = makeSlicedNode(hRemove, vRemove + 1, hPosition + hBox, vPosition + vBox);
+    const bottomRight = makeSlicedNode(hRemove + 1, vRemove, hPosition + hBox, vPosition);
+    const bottomLeft = makeSlicedNode(hPosition, vPosition, hRemove, vRemove - 1);
+
+    const nodes = [topLeft, topRight, bottomRight, bottomLeft];
+    const nonemptyNodes = nodes.filter(node => node.getVolume() > 0);
+
+    return nonemptyNodes;
   }
 }
 Node.fromCoords = ({startCoords, endCoords}) => {
