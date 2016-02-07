289a7bd93f7baba97803e6182c6e07d75bafa9d3
Avaer <a@kazmer.io>
2016-02-07 09:23:16 +0000

    Fix log textures definition

--- public/resources/blocks.js
+++ public/resources/blocks.js
@@ -2,12 +2,12 @@ const BLOCK_TEXTURES = require('./block-textures');
 
 const MULTI_BLOCK_TEXTURES = {
   'grass_top': ['grass_top', 'dirt', 'grass_side'],
-  'log_acacia': ['log_acacia_top', 'log_acacia', 'log_acacia_top'],
-  'log_big_oak': ['log_big_oak_top', 'log_big_oak', 'log_big_oak_top'],
-  'log_birch': ['log_birch_top', 'log_birch', 'log_birch_top'],
-  'log_jungle': ['log_jungle_top', 'log_jungle', 'log_jungle_top'],
-  'log_oak': ['log_oak_top', 'log_oak', 'log_oak_top'],
-  'log_spruce': ['log_spruce_top', 'log_spruce', 'log_spruce_top'],
+  'log_acacia': ['log_acacia_top', 'log_acacia_top', 'log_acacia'],
+  'log_big_oak': ['log_big_oak_top', 'log_big_oak_top', 'log_big_oak'],
+  'log_birch': ['log_birch_top', 'log_birch_top', 'log_birch'],
+  'log_jungle': ['log_jungle_top', 'log_jungle_top', 'log_jungle'],
+  'log_oak': ['log_oak_top', 'log_oak_top', 'log_oak'],
+  'log_spruce': ['log_spruce_top', 'log_spruce_top', 'log_spruce'],
 };
 
 export const BLOCKS = (() => {
