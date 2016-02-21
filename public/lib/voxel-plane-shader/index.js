6f05b5bde06d3877d74e4662cf1cfc7395423308
Avaer <a@kazmer.io>
2016-02-21 05:35:17 +0000

    Bugfix plane mesh glitching with custom shader

--- public/lib/voxel-plane-shader/index.js
+++ public/lib/voxel-plane-shader/index.js
@@ -56,14 +56,182 @@ function voxelPlaneShader(opts) {
   this.texture.magFilter = THREE.NearestFilter;
   this.texture.minFilter = THREE.LinearMipMapLinearFilter;
 
-  this.material = new THREE.MeshLambertMaterial({
-    ambient: 0xbbbbbb,
-    transparent: true,
-    side: THREE.FrontSide,
-    // lights: [], // force lights refresh to setup uniforms, three.js WebGLRenderer line 4323
-    fog: true,
-    map: this.texture
-  });
+  this.material = (() => {
+    const materialParams = {
+      ambient: 0xbbbbbb,
+      transparent: true,
+      side: THREE.FrontSide,
+      lights: [], // force lights refresh to setup uniforms, three.js WebGLRenderer line 4323
+      fog: true,
+
+      uniforms: THREE.UniformsUtils.merge( [
+
+        THREE.UniformsLib[ "common" ],
+        THREE.UniformsLib[ "aomap" ],
+        THREE.UniformsLib[ "lightmap" ],
+        THREE.UniformsLib[ "emissivemap" ],
+        THREE.UniformsLib[ "fog" ],
+        THREE.UniformsLib[ "ambient" ],
+        THREE.UniformsLib[ "lights" ],
+
+        {
+          "emissive" : { type: "c", value: new THREE.Color( 0x000000 ) }
+        }
+
+      ] ),
+
+      vertexShader: [
+
+        "#define LAMBERT",
+
+        "varying vec3 vLightFront;",
+
+        "#ifdef DOUBLE_SIDED",
+
+        "	varying vec3 vLightBack;",
+
+        "#endif",
+
+        THREE.ShaderChunk[ "common" ],
+        THREE.ShaderChunk[ "uv_pars_vertex" ],
+        THREE.ShaderChunk[ "uv2_pars_vertex" ],
+        THREE.ShaderChunk[ "envmap_pars_vertex" ],
+        THREE.ShaderChunk[ "bsdfs" ],
+        THREE.ShaderChunk[ "lights_pars" ],
+        THREE.ShaderChunk[ "color_pars_vertex" ],
+        THREE.ShaderChunk[ "morphtarget_pars_vertex" ],
+        THREE.ShaderChunk[ "skinning_pars_vertex" ],
+        THREE.ShaderChunk[ "shadowmap_pars_vertex" ],
+        THREE.ShaderChunk[ "logdepthbuf_pars_vertex" ],
+
+        "void main() {",
+
+          THREE.ShaderChunk[ "uv_vertex" ],
+          THREE.ShaderChunk[ "uv2_vertex" ],
+          THREE.ShaderChunk[ "color_vertex" ],
+
+          THREE.ShaderChunk[ "beginnormal_vertex" ],
+          THREE.ShaderChunk[ "morphnormal_vertex" ],
+          THREE.ShaderChunk[ "skinbase_vertex" ],
+          THREE.ShaderChunk[ "skinnormal_vertex" ],
+          THREE.ShaderChunk[ "defaultnormal_vertex" ],
+
+          THREE.ShaderChunk[ "begin_vertex" ],
+          THREE.ShaderChunk[ "morphtarget_vertex" ],
+          THREE.ShaderChunk[ "skinning_vertex" ],
+          THREE.ShaderChunk[ "project_vertex" ],
+          THREE.ShaderChunk[ "logdepthbuf_vertex" ],
+
+          THREE.ShaderChunk[ "worldpos_vertex" ],
+          THREE.ShaderChunk[ "envmap_vertex" ],
+          THREE.ShaderChunk[ "lights_lambert_vertex" ],
+          THREE.ShaderChunk[ "shadowmap_vertex" ],
+
+        "}"
+
+      ].join( "\n" ),
+
+      fragmentShader: [
+
+        "uniform vec3 diffuse;",
+        "uniform vec3 emissive;",
+        "uniform float opacity;",
+
+        "varying vec3 vLightFront;",
+
+        "#ifdef DOUBLE_SIDED",
+
+        "	varying vec3 vLightBack;",
+
+        "#endif",
+
+        THREE.ShaderChunk[ "common" ],
+        THREE.ShaderChunk[ "color_pars_fragment" ],
+        THREE.ShaderChunk[ "uv_pars_fragment" ],
+        THREE.ShaderChunk[ "uv2_pars_fragment" ],
+        THREE.ShaderChunk[ "map_pars_fragment" ],
+        THREE.ShaderChunk[ "alphamap_pars_fragment" ],
+        THREE.ShaderChunk[ "aomap_pars_fragment" ],
+        THREE.ShaderChunk[ "lightmap_pars_fragment" ],
+        THREE.ShaderChunk[ "emissivemap_pars_fragment" ],
+        THREE.ShaderChunk[ "envmap_pars_fragment" ],
+        THREE.ShaderChunk[ "bsdfs" ],
+        THREE.ShaderChunk[ "ambient_pars" ],
+        THREE.ShaderChunk[ "lights_pars" ],
+        THREE.ShaderChunk[ "fog_pars_fragment" ],
+        THREE.ShaderChunk[ "shadowmap_pars_fragment" ],
+        THREE.ShaderChunk[ "shadowmask_pars_fragment" ],
+        THREE.ShaderChunk[ "specularmap_pars_fragment" ],
+        THREE.ShaderChunk[ "logdepthbuf_pars_fragment" ],
+
+        "void main() {",
+
+        "	vec4 diffuseColor = vec4( diffuse, opacity );",
+        "	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );",
+        "	vec3 totalEmissiveLight = emissive;",
+
+          THREE.ShaderChunk[ "logdepthbuf_fragment" ],
+
+          // begin custom
+          // THREE.ShaderChunk[ "map_fragment" ],
+          'vec4 texelColor = texture2D(map, vUv);',
+
+          'if (texelColor.a < 0.5) discard;',
+
+          'texelColor.xyz = inputToLinear(texelColor.xyz);',
+
+          'diffuseColor *= texelColor;',
+          // end custom
+
+          THREE.ShaderChunk[ "color_fragment" ],
+          THREE.ShaderChunk[ "alphamap_fragment" ],
+          THREE.ShaderChunk[ "alphatest_fragment" ],
+          THREE.ShaderChunk[ "specularmap_fragment" ],
+          THREE.ShaderChunk[ "emissivemap_fragment" ],
+
+          // accumulation
+        "	reflectedLight.indirectDiffuse = getAmbientLightIrradiance( ambientLightColor );",
+
+          THREE.ShaderChunk[ "lightmap_fragment" ],
+
+        "	reflectedLight.indirectDiffuse *= BRDF_Diffuse_Lambert( diffuseColor.rgb );",
+
+        "	#ifdef DOUBLE_SIDED",
+
+        "		reflectedLight.directDiffuse = ( gl_FrontFacing ) ? vLightFront : vLightBack;",
+
+        "	#else",
+
+        "		reflectedLight.directDiffuse = vLightFront;",
+
+        "	#endif",
+
+        "	reflectedLight.directDiffuse *= BRDF_Diffuse_Lambert( diffuseColor.rgb ) * getShadowMask();",
+
+          // modulation
+          THREE.ShaderChunk[ "aomap_fragment" ],
+
+        "	vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + totalEmissiveLight;",
+
+          THREE.ShaderChunk[ "envmap_fragment" ],
+
+          THREE.ShaderChunk[ "linear_to_gamma_fragment" ],
+
+          THREE.ShaderChunk[ "fog_fragment" ],
+
+        "	gl_FragColor = vec4( outgoingLight, diffuseColor.a );",
+
+        "}"
+
+      ].join( "\n" )
+    };
+    materialParams.uniforms.map.value = this.texture;
+
+    const material = new THREE.ShaderMaterial(materialParams);
+    material.map = true;
+
+    return material;
+  })();
 
   // a place for meshes to wait while textures are loading
   this._meshQueue = [];
