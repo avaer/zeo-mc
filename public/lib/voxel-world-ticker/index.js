0f3485103fdd7e31aacb857ba0efb452c22465f6
Avaer <a@kazmer.io>
2016-03-09 08:09:10 +0000

    Computing top and bottom frontiers in voxel-world-tick module

--- public/lib/voxel-world-ticker/index.js
+++ public/lib/voxel-world-ticker/index.js
@@ -36,24 +36,37 @@ class VoxelWorldTicker {
       return null;
     };
 
-    const getBoundingBlob = (startPosition, predicate) => {
-      const result = [];
+    const getBlobFrontier = (startPosition, predicate) => {
+      const topFrontier = Array(chunkSize * chunkSize);
+      const bottomFrontier = Array(chunkSize * chunkSize);
+
+      function getPlaneIndex(x, z) {
+        return x + chunkSize * z;
+      }
+
       const frontier = [startPosition];
       const seenIndex = {};
       while (frontier.length > 0) {
         const position = frontier.shift();
-        const index = voxelUtils.getIndex(position[0], position[1], position[2]);
+        const [x, y, z] = position;
+        const index = voxelUtils.getIndex(x, y, z);
         if (!(index in seenIndex)) {
           if (predicate(position)) {
-            result.push(position);
+            const planeIndex = getPlaneIndex(x, z);
+            if (topFrontier[planeIndex] === undefined || y < topFrontier[planeIndex]) {
+              topFrontier[planeIndex] = y;
+            }
+            if (bottomFrontier[planeIndex] === undefined || y > bottomFrontier[planeIndex]) {
+              bottomFrontier[planeIndex] = y;
+            }
 
             [
-              [position[0] - 1, position[1], position[2]], // left
-              [position[0] + 1, position[1], position[2]], // right
-              [position[0], position[1] - 1, position[2]], // top
-              [position[0], position[1] + 1, position[2]], // bottom
-              [position[0], position[1], position[2] - 1], // back
-              [position[0], position[1], position[2] + 1], // front
+              [x - 1, y, z], // left
+              [x + 1, y, z], // right
+              [x, y - 1, z], // top
+              [x, y + 1, z], // bottom
+              [x, y, z - 1], // back
+              [x, y, z + 1], // front
             ].forEach(nextPosition => {
               if (isInBounds(nextPosition)) {
                 frontier.push(nextPosition);
@@ -64,13 +77,13 @@ class VoxelWorldTicker {
           seenIndex[index] = true;
         }
       }
-      return result;
+      return {top: topFrontier, bottom: bottomFrontier}; // XXX should compute a top max-heap and a bottom min-heap instead
     };
 
-    const isInBounds = position => (
-      position[0] >= 0 && position[0] < chunkSize &&
-      position[1] >= 0 && position[1] < chunkSize &&
-      position[2] >= 0 && position[2] < chunkSize
+    const isInBounds = ([x, y, z]) => (
+      x >= 0 && x < chunkSize &&
+      y >= 0 && y < chunkSize &&
+      z >= 0 && z < chunkSize
     );
 
     for (let i = 0; i < chunks.length; i++) {
@@ -79,12 +92,15 @@ class VoxelWorldTicker {
 
       const startPosition = getPositionWithValue(voxels, WATER_STILL);
       if (startPosition) {
-        const boundingBlob = getBoundingBlob(startPosition, position => {
+        const blobFrontier = getBlobFrontier(startPosition, position => {
           const index = voxelUtils.getIndex(position[0], position[1], position[2]);
           const value = voxels[index];
           return value === WATER_STILL;
         });
-        console.log('got bounding blob', {startPosition: startPosition.join(','), boundingBlob}); // XXX
+        if (!window.lol) { // XXX
+          console.log('got blob frontier', {startPosition: startPosition.join(','), blobFrontier});
+          window.lol = true;
+        }
       }
     }
   }
