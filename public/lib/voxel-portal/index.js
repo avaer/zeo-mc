354f911f752c9bf7a62987a67f5c6025529a3b21
Avaer <a@kazmer.io>
2016-03-19 06:31:32 +0000

    Use child portal camera for portals rendering

--- public/lib/voxel-portal/index.js
+++ public/lib/voxel-portal/index.js
@@ -110,6 +110,7 @@ function _makePortalRenderer(sourcePortalMesh, target1, target2, game) {
   const {width, height, scene, camera, view, THREE} = game;
 
   const portalCamera = new THREE.PerspectiveCamera(view.fov, view.aspectRatio, view.nearPlane, view.farPlane);
+  camera.add(portalCamera);
 
   const screenScene = (() => {
     const screenScene = new THREE.Scene();
@@ -132,22 +133,6 @@ function _makePortalRenderer(sourcePortalMesh, target1, target2, game) {
   function updatePortalCamera() {
     // reflecting a vector:
     // http://www.3dkingdoms.com/weekly/weekly.php?a=2
-
-    // copy portalCamera from main camera
-    const positionVector = new THREE.Vector3().setFromMatrixPosition(camera.matrixWorld);
-    portalCamera.position.copy(positionVector);
-
-    const upVector = camera.up.clone().applyMatrix4(camera.matrixWorld);
-    portalCamera.up.copy(upVector);
-
-    const rotationMatrix = new THREE.Matrix4().extractRotation(camera.matrixWorld);
-    const quaternion = new THREE.Quaternion().setFromRotationMatrix(rotationMatrix);
-    const lookAtVector = new THREE.Vector3(0, 0, -1).applyQuaternion(quaternion);
-    portalCamera.lookAt(lookAtVector);
-    // update portalCamera matrices
-    portalCamera.updateProjectionMatrix();
-    portalCamera.updateMatrixWorld();
-    portalCamera.matrixWorldInverse.getInverse(portalCamera.matrixWorld);
 
     /* // now update projection matrix with new clip plane
     // implementing code from: http://www.terathon.com/code/oblique.html
@@ -185,7 +170,7 @@ function _makePortalRenderer(sourcePortalMesh, target1, target2, game) {
   }
 
   function renderTargets() {
-    view.renderer.render(scene, camera, target1, true);
+    view.renderer.render(scene, portalCamera, target1, true);
     view.renderer.render(screenScene, screenCamera, target2, true)
   }
 
