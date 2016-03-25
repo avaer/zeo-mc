b7ef6bd686dcf42f18aec3aba4fecaf35b4a725b
Avaer <a@kazmer.io>
2016-03-25 06:54:25 +0000

    Fix graphql post handling

--- routes/graphql.js
+++ routes/graphql.js
@@ -1,16 +1,11 @@
 const expressGraphql = require('express-graphql');
 const Schema = require('../lib/graphql/schema');
 
-const handler = expressGraphql({ schema: Schema, pretty: true, graphiql: true /* XXX */ });
-
 const routes = [
   {
+    methods: ['get', 'post'],
     path: '/graphql',
-    handler: (req, res) => {
-      console.log('got req', req.url);
-
-      handler(req, res);
-    }
+    handler: expressGraphql({ schema: Schema, pretty: true, graphiql: true /* XXX */ })
   }
 ];
 
--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,21 @@ const api = {
     const app = express();
 
     allRoutes.forEach(route => {
-      const method = route.method || 'get';
+      const methods = (() => {
+        if (route.method) {
+          return [route.method];
+        } else if (route.methods) {
+          return route.methods;
+        } else {
+          return ['get'];
+        }
+      })();
       const path = route.path;
       const handler = Array.isArray(route.handler) ? route.handler : [ route.handler ];
 
-      app[method](path, handler);
+      methods.forEach(method => {
+        app[method](path, handler);
+      });
     });
 
     return app;
