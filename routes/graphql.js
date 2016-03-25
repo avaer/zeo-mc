522759d20ae4eed71bcc978ece4a0164eea7ee1d
Avaer <a@kazmer.io>
2016-03-25 05:39:13 +0000

    Working on graphql route integration

--- routes/graphql.js
+++ routes/graphql.js
@@ -1,36 +1,12 @@
-const path = require('path');
-const url = require('url');
+const expressGraphql = require('express-graphql');
+const Schema = require('../lib/graphql/schema');
+const db = require('../lib/graphql/database');
 
-/* const st = require('st');
-
-const stFavicon = st({
-  path: path.join(__dirname, '..'),
-  url: '/'
-});
-stFavicon.url = '/favicon.ico';
-const stJs = st({
-  path: path.join(__dirname, '..', 'public', 'js'),
-  url: '/js'
-});
-stJs.url = '/js/:path*';
-const stImg = st({
-  path: path.join(__dirname, '..', 'public', 'img'),
-  url: '/img'
-});
-stImg.url = '/img/:path*';
-
-const stMounts = [stFavicon, stJs, stImg];
-
-const routes = stMounts.map(stMount => ({
-  path: stMount.url,
-  handler: (req, res, next) => {
-    const stHandled = stMount(req, res);
-    if (!stHandled) {
-      next();
-    }
+const routes = [
+  {
+    path: '/graphql/:path*',
+    handler: expressGraphql({ schema: Schema, pretty: true, graphiql: true })
   }
-})); */
-
-const routes = [];
+];
 
 module.exports = routes;
