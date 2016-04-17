8ed54aaec788e7ef2db945950f38cc990101f0d4
Avaer <a@kazmer.io>
2016-04-17 03:17:56 -0700

    Add large house village metadata

--- metadata/villages/index.js
+++ metadata/villages/index.js
@@ -1,12 +1,14 @@
 const Farm = require('./Farm');
 const Well = require('./Well');
 const HouseSmall = require('./HouseSmall');
+const HouseLarge = require('./HouseLarge');
 const Blacksmith = require('./Blacksmith');
 
 const BUILDINGS = [
   Farm,
   Well,
   HouseSmall,
+  HouseLarge,
   Blacksmith,
 ];
 
