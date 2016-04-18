ac474e1f6345237eba3cf7e98a9726fa62c06620
Avaer <a@kazmer.io>
2016-04-17 20:27:51 -0700

    Initial per-tree type constants in trees metadata

--- metadata/trees/index.js
+++ metadata/trees/index.js
@@ -3,14 +3,16 @@
 const Blocks = require('../blocks/index');
 const BLOCKS = Blocks.BLOCKS;
 
-const TREE_MIN_HEIGHT = 4;
-const TREE_MAX_HEIGHT = 14;
-const TREE_BASE_MIN_RATIO = 0.1;
-const TREE_BASE_MAX_RATIO = 0.7;
-const TREE_LEAF_RATE = 0.5;
-const TREE_LEAF_SIZE = 2;
+const OAK_MIN_HEIGHT = 4;
+const OAK_MAX_HEIGHT = 14;
+const OAK_BASE_MIN_RATIO = 0.1;
+const OAK_BASE_MAX_RATIO = 0.7;
+const OAK_LEAF_RATE = 0.5;
+const OAK_LEAF_SIZE = 2;
+const OAK_LOG_VALUE = BLOCKS['log_big_oak'];
+const OAK_LEAVES_VALUE = BLOCKS['leaves_big_oak_plains'];
 
-const TREE_DIRECTIONS = (function() {
+const OAK_DIRECTIONS = (function() {
   var result = [];
   for (let x = -1; x <= 1; x++) {
     for (let y = -1; y <= 1; y++) {
@@ -24,9 +26,6 @@ const TREE_DIRECTIONS = (function() {
   }
   return result;
 })();
-
-const LOG_VALUE = BLOCKS['log_big_oak'];
-const LEAVES_VALUE = BLOCKS['leaves_big_oak_plains'];
 
 const floor = Math.floor;
 const sqrt = Math.sqrt;
@@ -47,13 +46,13 @@ const TREES = [
     const voxelUtils = opts.voxelUtils;
 
     const heightNoiseN = heightNoise.in2D(x, z);
-    const height = TREE_MIN_HEIGHT + (heightNoiseN * (TREE_MAX_HEIGHT - TREE_MIN_HEIGHT));
+    const height = OAK_MIN_HEIGHT + (heightNoiseN * (OAK_MAX_HEIGHT - OAK_MIN_HEIGHT));
     const baseNoiseN = baseNoise.in2D(x, z);
-    const base = height * (TREE_BASE_MIN_RATIO + (baseNoiseN * (TREE_BASE_MAX_RATIO - TREE_BASE_MIN_RATIO)));
+    const base = height * (OAK_BASE_MIN_RATIO + (baseNoiseN * (OAK_BASE_MAX_RATIO - OAK_BASE_MIN_RATIO)));
 
     function leafPoints(fn) {
-      for (let j = -TREE_LEAF_SIZE; j <= TREE_LEAF_SIZE; j++) {
-        for (let k = -TREE_LEAF_SIZE; k <= TREE_LEAF_SIZE; k++) {
+      for (let j = -OAK_LEAF_SIZE; j <= OAK_LEAF_SIZE; j++) {
+        for (let k = -OAK_LEAF_SIZE; k <= OAK_LEAF_SIZE; k++) {
           if (j === 0 && k === 0) continue;
           fn(j, k);
         }
@@ -62,7 +61,7 @@ const TREES = [
 
     for (let i = 0; i < height; i++) {
       let yi = y + i;
-      onPoint(x, yi, z, LOG_VALUE);
+      onPoint(x, yi, z, OAK_LOG_VALUE);
 
       if (i >= base) {
         const leafSets = {};
@@ -72,7 +71,7 @@ const TREES = [
 
           const leafN = leafNoise.in3D(xi, yi, zi);
           const leafDistance = sqrt(j * j + k * k);
-          const leafProbability = TREE_LEAF_RATE - ((leafDistance - 1) / (TREE_LEAF_SIZE - 1)) * TREE_LEAF_RATE;
+          const leafProbability = OAK_LEAF_RATE - ((leafDistance - 1) / (OAK_LEAF_SIZE - 1)) * OAK_LEAF_RATE;
           if (leafN < leafProbability) {
             const idx = voxelUtils.getIndex(xi, yi, zi);
             leafSets[idx] = true;
@@ -82,7 +81,7 @@ const TREES = [
           const xi = x + j;
           const zi = z + k;
 
-          if (TREE_DIRECTIONS.some(function(d) {
+          if (OAK_DIRECTIONS.some(function(d) {
             const idx = voxelUtils.getIndex(xi + d.x, yi + d.y, zi + d.z);
             return !!leafSets[idx];
           })) {
@@ -95,7 +94,7 @@ const TREES = [
     const snappedHeight = floor(height);
     const yi = y + snappedHeight;
     const tipTreeLeafN = leafNoise.in3D(x, yi, z);
-    if (tipTreeLeafN < TREE_LEAF_RATE) {
+    if (tipTreeLeafN < OAK_LEAF_RATE) {
       onPoint(x, yi, z, LEAVES_VALUE);
     }
   },
