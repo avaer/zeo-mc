cd59a73e24fe5a73301ea592a7112b02f2d1498a
Avaer <a@kazmer.io>
2016-04-24 14:49:00 -0700

    Working on hooking in village generation

--- metadata/villages/index.js
+++ metadata/villages/index.js
@@ -8,6 +8,9 @@ const ButcherShop = require('./ButcherShop');
 const Church = require('./Church');
 const Library = require('./Library');
 
+const VILLAGE_RATE_PER_CHUNK = 0.1;
+const WELL_OFFSET = [2, 2];
+
 const BUILDINGS = [
   Farm,
   Well,
@@ -28,11 +31,41 @@ const BUILDING_INDEX = (() => {
   return result;
 })();
 
-function makeBuilding(buildingType, opts) {
-  const {type, position, onPoint} = opts;
+function make(opts) {
+  const position = opts.position;
+  const chunkNoise = opts.chunkNoise;
+  const wellNoiseX = opts.wellNoiseX;
+  const wellNoiseZ = opts.wellNoiseZ;
+  const onPoint = opts.onPoint;
+
+  const chunkX = position.chunkX;
+  const chunkZ = position.chunkZ;
+
+  const chunkNoiseN = chunkNoise.in2D(chunkX, chunkZ);
+  if (chunkNoiseN < VILLAGE_RATE_PER_CHUNK) {
+    const wellNoiseXN = wellNoiseX.in2D(chunkX, chunkZ);
+    const wellX = startX + floor(wellNoiseXN * chunkSize) + WELL_OFFSET[0];
+    const wellNoiseZN = wellNoiseZ.in2D(chunkX, chunkZ);
+    const wellZ = startZ + floor(villageWellNoiseZN * chunkSize) + WELL_OFFSET[1];
+    // XXX ensure there is room for the well before trying to make it
+    VILLAGES.makeBuilding({
+      type: 'well',
+      position: [wellX, wellZ],
+      onPoint
+    });
+    // XXX generate worms and additional buildings in NEWS directions
+  }
+}
+
+function _makeBuilding(buildingType, opts) {
+  const type = opts.type;
+  const position = opts.position;
+  const onPoint = opts.onPoint;
+
   const Building = BUILDING_INDEX[type];
   const building = new Building();
-  const {layers, offset} = building;
+  const layers = building.layers;
+  const offset = building.offset;
   // XXX place the building on the highest terrain height of the horizontal blocks it occupies
   // XXX generate a stone base for the building if needed
   // XXX reify layers to onPoint() block value calls
@@ -41,7 +74,7 @@ function makeBuilding(buildingType, opts) {
 
 const api = {
   BUILDINGS,
-  makeBuilding,
+  make,
 };
 
 module.exports = api;
