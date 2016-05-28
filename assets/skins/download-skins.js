31599287ef4d3d66e5992eed6d0d302444165b79
Adrian Biedrzycki <a@modules.io>
2016-05-28 16:42:28 -0700

    More download skin script tweaking

--- assets/skins/download-skins.js
+++ assets/skins/download-skins.js
@@ -4,96 +4,115 @@ const path = require('path');
 const fs = require('fs');
 const request = require('request');
 const mkdirp = require('mkdirp');
+const htmlToText = require('html-to-text');
 
 const PNG_DIRECTORY = path.join(__dirname, 'png');
 const START_SKIN_PAGE = 0;
 const END_SKIN_PAGE = 1000;
 
-function recursePages(cb) {
+function recursePages(startSkinPage, endSkinPage, cb) {
   const map = {};
 
   (function recursePage(i) {
     if (i <= END_SKIN_PAGE) {
-      console.log('------------');
-      console.log('PAGE ' + i);
-      console.log('------------');
+      (function retry() {
+        console.log('------------');
+        console.log('PAGE ' + i);
+        console.log('------------');
 
-      request.get({
-        url: 'http://www.minecraftskins.com/latest/' + i + '/',
-        encoding: 'utf8',
-      }, (err, res, body) => {
-        const skinSpecs = (() => {
-          const result = [];
-          let regexp = /<a href=".*?\/skin\/[^\/]+\/(.+?)\/"[^>]*>\s*<div[^>]*data-url=".*?\/newuploaded_skins\/(.+?)\.png"/g, match;
-          while (match = regexp.exec(body)) {
-            const name = match[1];
-            const id = match[2];
-            if (!map[id]) {
-              map[id] = true;
-              result.push({
-                name,
-                id,
-              });
-            }
-          }
-          return result;
-        })();
+        request.get({
+          url: 'http://www.minecraftskins.com/latest/' + i + '/',
+          encoding: 'utf8',
+        }, (err, res, body) => {
+          const text = htmlToText.fromString(body);
 
-        const next = () => {
-          recursePage(i + 1);
-        };
-
-        if (skinSpecs.length > 0) {
-          let pending = skinSpecs.length;
-          let error = null;
-          const pend = err => {
-            error = error || err;
-            if (--pending === 0) {
-              if (!error) {
-                next();
-              } else {
-                cb(error);
+          const skinSpecs = (() => {
+            const result = [];
+            const regexp = /\[.*?\/([^\/]+?)\/\]\s*\[.*?\/newuploaded_skins\/([^\/]+?)\.png\]/g;
+            let match;
+            while (match = regexp.exec(text)) {
+              const name = match[1];
+              const id = match[2];
+              if (!map[id]) {
+                map[id] = true;
+                result.push({
+                  name,
+                  id,
+                });
               }
             }
+            return result;
+          })();
+
+          const next = () => {
+            recursePage(i + 1);
           };
 
-          skinSpecs.forEach(skinSpec => {
-            const name = skinSpec.name;
-            const id = skinSpec.id;
-
-            const description = name + ' (' + id + ')';
-            console.log(description);
-            const req = request.get({
-              url: 'http://www.minecraftskins.com/downloadnew.php?file=newuploaded_skins/' + id + '.png',
-            });
-            req.on('response', res => {
-              if (res.statusCode >= 200 && res.statusCode < 300 && res.headers['content-type'] !== 'text/html') {
-                const skinFilePath = path.join(PNG_DIRECTORY, name + '-' + id + '.png');
-                const ws = fs.createWriteStream(skinFilePath);
-                res.pipe(ws);
-
-                ws.on('error', err => { pend(err); });
-                ws.on('finish', () => { pend(); });
-              } else {
-                console.warn('FAILED: ' + description);
-                pend();
+          if (skinSpecs.length > 0) {
+            let pending = skinSpecs.length;
+            let error = null;
+            const pend = err => {
+              error = error || err;
+              if (--pending === 0) {
+                if (!error) {
+                  next();
+                } else {
+                  cb(error);
+                }
               }
+            };
+
+            skinSpecs.forEach(skinSpec => {
+              (function retry() {
+                const name = skinSpec.name;
+                const id = skinSpec.id;
+
+                const description = name + ' (' + id + ')';
+                console.log(description);
+                const req = request.get({
+                  url: 'http://www.minecraftskins.com/downloadnew.php?file=newuploaded_skins/' + id + '.png',
+                  timeout: 5000
+                });
+                req.on('response', res => {
+                  if (res.statusCode >= 200 && res.statusCode < 300 && res.headers['content-type'] !== 'text/html') {
+                    const skinFilePath = path.join(PNG_DIRECTORY, name + '-' + id + '.png');
+                    const ws = fs.createWriteStream(skinFilePath);
+                    res.pipe(ws);
+
+                    ws.on('error', err => { pend(err); });
+                    ws.on('finish', () => { pend(); });
+                  } else {
+                    console.warn('FAILED: ' + description);
+                    pend();
+                  }
+                });
+                req.on('error', err => {
+                  if (err.code === 'ETIMEDOUT' || err.code === 'ESOCKETTIMEDOUT') {
+                    retry();
+                  } else {
+                    pend(err);
+                  }
+                });
+              })();
             });
-            req.on('error', err => { pend(err); });
-          });
-        } else {
-          next();
-        }
-      });
+          } else {
+            console.log('no skins', text.length);
+
+            retry();
+          }
+        });
+      })();
     } else {
       cb();
     }
-  })(START_SKIN_PAGE);
+  })(startSkinPage);
 }
 
 mkdirp(PNG_DIRECTORY, err => {
   if (!err) {
-    recursePages(err => {
+    const startSkinPage = parseInt(process.argv[2], 10) || START_SKIN_PAGE;
+    const endSkinPage = parseInt(process.argv[3], 10) || END_SKIN_PAGE;
+    recursePages(startSkinPage, endSkinPage, err => {
       if (!err) {
         console.warn('done');
       } else {
